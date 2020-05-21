7b8b47916dbfda4ce4398426dd6f57a3f5ca3138
cessy <cessycode@gmail.com>
2020-05-21 06:34:09 -0400

    modified fetch api base route

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends Component {
 
     onSubmitSignIn = (event) => {
         event.preventDefault();
-        fetch('http://localhost:3000/register', {
+        fetch('https://facescanner-api.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
